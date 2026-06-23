9cc0c7e6e97e88e8c95c9f5fae23aa601ce63d8d
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-23 15:12:56 +0500

    Update Inbox.jsx

--- frontend/src/components/Inbox/Inbox.jsx
+++ frontend/src/components/Inbox/Inbox.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import socketIO from "socket.io-client";
 import { formatDistanceToNow } from "date-fns";
-import { server } from "../../server";`r`nimport { getImageUrl } from "../../utils/imageUrl";
+import { server } from "../../server";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
