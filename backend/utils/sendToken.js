decc3b1242278315ea9f4e2ffc323a6e6ca84f81
Muhammad Haseeb <haseebmuneer333@gmail.com>
2026-06-23 11:32:58 +0500

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -4,7 +4,7 @@ const sendToken=async (user , statusCode , res)=>{
     const options={
         expires : new Date(Date.now()+90*24*60*60*1000),
         httpOnly:true,
-        sameSite: "lax", // Critical for cross-origin cookies
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",// Critical for cross-origin cookies
         secure: process.env.NODE_ENV === "production" ? true : false,   // Required if sameSite is "none"
     }
     res.status(statusCode).cookie("token" , token , options).json({
